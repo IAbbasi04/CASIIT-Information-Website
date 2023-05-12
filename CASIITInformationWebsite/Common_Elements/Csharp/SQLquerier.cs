7e6ae7b1f82dc98c9f24ae8e62a79fcc46e436e8
Chris Wall <offthewalls.oodie@gmail.com>
2023-05-12 08:15:38 -0400

    Added SQLquerier class
    Allows better interfacing with database through static class

---
+++ CASIITInformationWebsite/Common_Elements/Csharp/SQLquerier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace CASIITInformationWebsite.Common_Elements.Csharp
+{
+
+    public static class SQLquerier
+    {
+
+    }
+}
