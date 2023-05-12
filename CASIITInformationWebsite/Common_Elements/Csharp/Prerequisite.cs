99a89a684fc8b1b005f37d03af093cd4b1238b48
Chris Wall <offthewalls.oodie@gmail.com>
2023-05-12 09:32:50 -0400

    Moved ClassOption into its own file and and added toString to Prerequisites

--- CASIITInformationWebsite/Common_Elements/Csharp/Prerequisite.cs
+++ CASIITInformationWebsite/Common_Elements/Csharp/Prerequisite.cs
@@ -5,8 +5,8 @@ using System.Linq;
 using System.Web;
 using System.IO;
 using Org.BouncyCastle.Asn1.Cmp;
-using static CASIITInformationWebsite.Common_Elements.Csharp.Prerequisite.ClassOption;
 using System.Transactions;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CASIITInformationWebsite.Common_Elements.Csharp
 {
@@ -14,70 +14,27 @@ namespace CASIITInformationWebsite.Common_Elements.Csharp
     {
         private double min_GPA;
         private int min_year;
-        private string classes_required  = "no classes required"; //Class ID
-
-        public abstract class ClassOption
-        {
-            public class Option : ClassOption
-            {
-                Class c;
-                public Option( Class c)
-                {
-                    this.c = c;
-                }
-
-                override public string toString()
-                {
-                    return c.toString();
-                }
-                
-            }
-
-            public class And : ClassOption
-            {
-                private ClassOption ClassOption1;
-                private ClassOption ClassOption2;
-
-                public And(ClassOption classOption1, ClassOption classOption2)
-                {
-                    ClassOption1 = classOption1;
-                    ClassOption2 = classOption2;
-                }
-
-                override public string toString()
-                {
-                    return "(" + ClassOption1 + " AND " + ClassOption2 + ")";
-                }
-            }
-            public class Or : ClassOption
-            {
-                private ClassOption ClassOption1;
-                private ClassOption ClassOption2;
-
-                public Or(ClassOption classOption1, ClassOption classOption2)
-                {
-                    ClassOption1 = classOption1;
-                    ClassOption2 = classOption2;
-                }
-
-                
-                override public string toString()
-                {
-                    return "(" + ClassOption1 + " OR " + ClassOption2 + ")";
-                }
-            }
-
-            public abstract string toString();
-        }
+        private ClassOption required_classes;
+       
         // new Or( new And( new ClassOption(ENG101), new ClassOption(DB101) ), new And(  new ClassOption(ENG101), new ClassOption(DB101) ); 
         //ex classes required
         // ((ENG101 AND DB101) OR (ENG201 AND DB201))
-        public Prerequisite(double min_GPA, int min_year, string classes_required)
+        public Prerequisite(double min_GPA, int min_year, ClassOption required_classes)
         {
             this.min_GPA = min_GPA;
             this.min_year = min_year;
-            this.classes_required = classes_required;
+            this.required_classes = required_classes;
         }
+
+        public string toString()
+        {
+            return
+                "Min_GPA: " + min_GPA +
+                " Min_year: " + min_year +
+                " required_classes: " + required_classes;
+        }
+
+
 
     }
 }
