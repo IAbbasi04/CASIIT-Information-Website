b2722141b73991e55130a0c6e7f049c7164ea5fa
AidenAquarium <aaquino510@gmail.com>
2023-05-16 13:22:44 -0400

    Testing things
    Testing abstract class concept for User classes

---
+++ CASIITInformationWebsite/Common_Elements/Csharp/UserInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CASIITInformationWebsite.Common_Elements.Csharp
+{
+	public abstract class UserInfo
+	{
+		private string Name;
+		private int UserId;
+
+		public UserInfo (string name, int userId)
+        {
+			this.Name = name;
+			this.UserId = userId;
+        }
+	}
+
+	public class TestStudent : UserInfo
+    {
+		private int Year;
+		private double GPA;
+		private int CounselorId;
+
+		public TestStudent(string name, int userId, int year, double gpa, int counselorId)
+        {
+			this.Name = name;
+			this.UserId = userId;
+			this.Year = year;
+			this.GPA = gpa;
+			this.CounselorId = counselorId;
+        }
+
+		public string getInfo()
+        {
+			Console.WriteLine("Name: " + Name + "\nID: " + UserId + "\nGrade: " + Year + "\nGPA: " + GPA + "Counselor ID: " + CounselorId);
+        }
+    }
+
+}
--- JSON builder playgroud/Program.cs
+++ JSON builder playgroud/Program.cs
@@ -1,4 +1,6 @@
 ﻿using CASIITInformationWebsite;
 using CASIITInformationWebsite.Common_Elements.Csharp;
+using System.Diagnostics;
 
-Console.Out.WriteLine("balls");
+TestStudent test = new TestStudent("John Smith", 1209387, 9, 3.991, 21938);
+Console.WriteLine(test.getInfo());
