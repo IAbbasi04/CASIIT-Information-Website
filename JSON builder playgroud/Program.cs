614cd36f8b87c56fe96b052550d4d4de061ca9b2
AidenAquarium <aaquino510@gmail.com>
2023-05-16 13:22:44 -0400

    Revert "Testing things"
    This reverts commit b2722141b73991e55130a0c6e7f049c7164ea5fa.

--- JSON builder playgroud/Program.cs
+++ JSON builder playgroud/Program.cs
@@ -1,6 +1,4 @@
 ﻿using CASIITInformationWebsite;
 using CASIITInformationWebsite.Common_Elements.Csharp;
-using System.Diagnostics;
 
-TestStudent test = new TestStudent("John Smith", 1209387, 9, 3.991, 21938);
-Console.WriteLine(test.getInfo());
+Console.Out.WriteLine("balls");
