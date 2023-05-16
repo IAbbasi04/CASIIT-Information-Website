1924cab73c6908cc208056d2101e987282bf0834
NathanByun <byunnj1968@gmail.com>
2023-05-16 13:59:15 -0400

    GPA and Concentration FIlter

--- CASIITInformationWebsite/Pages/CourseCatalogue.aspx
+++ CASIITInformationWebsite/Pages/CourseCatalogue.aspx
@@ -1,5 +1,13 @@
 ﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CourseCatalogue.aspx.cs" Inherits="CASIITInformationWebsite.Contact" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-   
+    <asp:DropDownList ID="DropDownList1" runat="server">
+    </asp:DropDownList>
+
+    <asp:Label ID="Label1" runat="server" Text="Enter GPA">
+    </asp:Label>
+
+    <asp:TextBox ID="TextBox1" runat="server">
+    </asp:TextBox>
+
 </asp:Content>
--- CASIITInformationWebsite/Pages/CourseCatalogue.aspx.cs
+++ CASIITInformationWebsite/Pages/CourseCatalogue.aspx.cs
@@ -11,7 +11,25 @@ namespace CASIITInformationWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                PopulateDropDownList();
+            }
         }
+        private void PopulateDropDownList()
+        {
+            DropDownList1.Items.Clear();
+            DropDownList1.Items.Add(new ListItem("Concentrations", ""));
+            DropDownList1.Items.Add(new ListItem("Interactive Technology", "1"));
+            DropDownList1.Items.Add(new ListItem("Applied Sciences", "2"));
+            DropDownList1.Items.Add(new ListItem("Information Technology", "3"));
+        }
+        protected void ddlMyComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string filterValue = DropDownList1.SelectedValue;
+            // Replace this with your own code to filter your data.
+            // You can use a SQL query or LINQ to filter your data.
+        }
+
     }
 }
