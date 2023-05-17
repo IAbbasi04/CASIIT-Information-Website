30291775a1804e3e2afca020f1822b3d61e651f1
Megan Farran <farranme23@pwcs-edu.org>
2023-05-17 14:08:26 -0400

    Made changes to the web.config file to allow the project to be published

--- JSON builder playgroud/Program.cs
+++ JSON builder playgroud/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CASIITInformationWebsite;
+using CASIITInformationWebsite.Common_Elements;
 using CASIITInformationWebsite.Common_Elements.Csharp;
 using System.Diagnostics;
 
-TestStudent test = new TestStudent("John Smith", 1209387, 9, 3.991, 21938);
-Console.WriteLine(test.getInfo());
+Student test = new Student("John Smith", 1209387, 9, 3.991, 21938);
+//Console.WriteLine(test.getId());
